89d3a7db76661afdfb979ea17b9ddf918f2c7b6c
dev-nirob1 <dev.nirob1@gmail.com>
2026-09-22 20:48:30 +0600

    auth context

---
+++ src/app/context/AuthContext.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const logout = async () => {
+    await axios.post("/api/auth/logout");
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        const res = await axios.get("/api/auth/me");
+        setUser(res.data);
+      } catch {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
