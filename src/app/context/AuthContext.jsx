a4f7be9d7718e19e58cf26fc9ef4c7439ef2351e
dev-nirob1 <dev.nirob1@gmail.com>
2026-09-22 23:51:41 +0600

    authcontext issue solved

--- src/app/context/AuthContext.jsx
+++ src/app/context/AuthContext.jsx
@@ -2,16 +2,19 @@
 
 import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   const logout = async () => {
     await axios.post("/api/auth/logout");
     setUser(null);
+    router.push("/");
   };
 
   useEffect(() => {
